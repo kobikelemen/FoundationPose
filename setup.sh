41594e38723e9cb3c8ae109aa0b3c063a5420bc4
Kobi Kelemen <kobi.kelemen20@gmail.com>
2025-06-30 15:02:35 -0700

    fix

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ apt update && apt install -y unzip
 
 mkdir demo_data
 cd demo_data
-gdown --folder https://drive.google.com/drive/folders/1DFezOAD0oD1BblsXVxqDsl8fj0qzB82i
-gdown --folder https://drive.google.com/drive/folders/1pRyFmxYXmAnpku7nGRioZaKrVJtIsroP
+gdown --folder https://drive.google.com/drive/folders/1pRyFmxYXmAnpku7nGRioZaKrVJtIsroP # their example data
 
 # Extract the kinect driller sequence
 unzip kinect_driller_seq.zip
@@ -15,6 +14,7 @@ unzip kinect_driller_seq.zip
 # Extract the mustard archive
 unzip mustard0.zip
 cd ..
-gdown --folder https://drive.google.com/drive/folders/1yMvJmVE07lllLWLys5ASk-_UySep1Dxu
+gdown --folder https://drive.google.com/drive/folders/1yMvJmVE07lllLWLys5ASk-_UySep1Dxu # kobi data
+gdown --folder https://drive.google.com/drive/folders/1DFezOAD0oD1BblsXVxqDsl8fj0qzB82i # model weights
 
 mv no_diffusion/ weights
