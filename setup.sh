a44c4ed26a02675774817f19d286dd5bbd2a425c
kobikelemen <kobi.kelemen20@gmail.com>
2025-06-30 15:27:02 -0700

    fix

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ unzip kinect_driller_seq.zip
 # Extract the mustard archive
 unzip mustard0.zip
 cd ..
-gdown --folder https://drive.google.com/drive/folders/1yMvJmVE07lllLWLys5ASk-_UySep1Dxu # kobi data
+gdown 1OQDY6EUccAVD2pLiIOcDZYXkexMSCZWv --folder -O ./data # kobi data
 gdown --folder https://drive.google.com/drive/folders/1DFezOAD0oD1BblsXVxqDsl8fj0qzB82i # model weights
 
 mv no_diffusion/ weights
