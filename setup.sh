cefadaa0e4dd42908cc1905ca6781699ef1a84ae
Kobi Kelemen <kobi.kelemen20@gmail.com>
2025-06-29 18:54:39 -0700

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+bash build_all.sh
+
+
+
+gdown --folder https://drive.google.com/drive/folders/1DFezOAD0oD1BblsXVxqDsl8fj0qzB82i
+gdown --folder https://drive.google.com/drive/folders/1pRyFmxYXmAnpku7nGRioZaKrVJtIsroP
+
+
+apt update && apt install -y unzip
+
+# Or if you prefer using apt-get
+apt-get update && apt-get install -y unzip
+
+cd demo_data
+# Extract the kinect driller sequence
+unzip kinect_driller_seq.zip
+
+# Extract the mustard archive
+unzip mustard0.zip
+
+
