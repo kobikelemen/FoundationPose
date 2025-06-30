dd2b394c6d3112440014b60a81af7e991a98f42c
Kobi Kelemen <kobi.kelemen20@gmail.com>
2025-06-30 09:52:34 -0700

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 bash build_all.sh
 
 
-
-gdown --folder https://drive.google.com/drive/folders/1DFezOAD0oD1BblsXVxqDsl8fj0qzB82i
-gdown --folder https://drive.google.com/drive/folders/1pRyFmxYXmAnpku7nGRioZaKrVJtIsroP
-gdown --folder https://drive.google.com/drive/folders/1yMvJmVE07lllLWLys5ASk-_UySep1Dxu
-
+pip install gdown
 apt update && apt install -y unzip
 
-# Or if you prefer using apt-get
-apt-get update && apt-get install -y unzip
-
 cd demo_data
+gdown --folder https://drive.google.com/drive/folders/1DFezOAD0oD1BblsXVxqDsl8fj0qzB82i
+gdown --folder https://drive.google.com/drive/folders/1pRyFmxYXmAnpku7nGRioZaKrVJtIsroP
+
 # Extract the kinect driller sequence
 unzip kinect_driller_seq.zip
 
 # Extract the mustard archive
 unzip mustard0.zip
-
+cd ..
+gdown --folder https://drive.google.com/drive/folders/1yMvJmVE07lllLWLys5ASk-_UySep1Dxu
 
